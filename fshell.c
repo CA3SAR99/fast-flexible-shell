6479eb34062d7011f332c52630571d84b93040cf
Cesare Landi <cesare.landi.99@gmail.com>
2025-06-01 21:36:18 +0200

    Another commit

--- fshell.c
+++ fshell.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#define MAX_INPUT 1024
+
+
 
 int main (void) {
-    while (1) {
+    char input[MAX_INPUT];
+
+    while(1) {
         printf("fsh>: ");
+        fgets(input,MAX_INPUT,stdin);
     }
     return 0;
 }
-
 // work in progess run at your own risk 
