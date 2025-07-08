a72c2c633abf9d7b3b83d57c3dfa31bbfb254faf
Cesare Landi <cesare.landi.99@gmail.com>
2025-07-08 20:37:42 +0200

    Changed some logic and renamed files

--- ffsh.c
+++ ffsh.c
@@ -9,17 +9,21 @@ int main (void) {
     char input[MAX_INPUT];
 
     while(1) {
-        printf("fsh>: ");
+        printf("ffsh>: ");
         fgets(input,MAX_INPUT,stdin);
 
         input[strcspn(input, "\n")] = 0;
+
+        // if input is empty just ignore the command
+        if (input[0] == '\0') {
+            continue;
+            }
 
         if (strcmp(input, "exit") == 0) {
             printf("Exiting... \n");
             break;
         }
-
-        printf("Unknown command: %s \n", input);
+        
     }
     return 0;
 }
